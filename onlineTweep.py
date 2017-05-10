8ce335b26d62d3ad0ac6cc8eb81986e9f78fb546
Rohoolio <rowan_gill@hotmail.co.uk>
2017-05-10 10:32:20 +0100

    Add files via upload

---
+++ onlineTweep.py
@@ -0,0 +1,141 @@
+import sys
+import tweepy
+import csv
+import time
+import pandas as pd
+import numpy as np
+
+# These are the custom access and consumer codes that allows access to
+# the twitter stream and process the data obtained.
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+
+# This authorises the user using the access and consumer details with tweepy so
+# that it can access the data and use its API.
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+# This is the stream listener that connects to twitter, this is where the original
+# twitter API is overridden so that we can manipulate the data to our needs.
+class CustomStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        self.api = api
+        # This starts a self timer at 0, this will be used to count the number of tweets collected.
+        self.n = 0
+        # This will be the number of tweets that the streamer will collect before it stops collecting.
+        self.m = 30
+
+        # Create a file with 'data_' and the current time
+        self.filename = 'data' + '_' + time.strftime('%Y%m%d%H%M%S') + '.csv'
+        # Create a new file with that filename
+        csvFile = open(self.filename, 'w')
+
+        # Create a csv writer
+        # Line terminator prevents the program adding unwanted extra lines that can upset later functions.
+        csvWriter = csv.writer(csvFile, lineterminator = '\n')
+
+        # Write a single row with the headers of the columns
+        csvWriter.writerow(['user.screen_name',
+                            'text',
+                            'coordinates'])
+
+
+    # This changes the on_data method of a stream listener which receives all messages and calls functions according to the message type
+    # def on_data(self, data):
+    #   all_data = json.loads(data)
+
+    #    with open('Tweets.txt', 'a', encoding='utf-8') as f:
+    #        f.write(data)
+    #        self.n = self.n + 1
+    #        if self.n < self.m:
+    #            print('tweets = ' + str(self.n))
+    #            return True
+    #        else:
+    #           print('finished')
+    #           return False
+
+    # When a tweet appears.
+    def on_status(self, status):
+
+        # Open the csv file created previously.
+        csvFile = open(self.filename, 'a')
+
+        # Create a csv writer.
+        # Line terminator will prevent a new line from being added every time.
+        csvWriter = csv.writer(csvFile, lineterminator='\n')
+
+        # If the tweet is not a retweet.
+        if not 'RT @' in status.text:
+            # Try to
+            try:
+                # This counts up the counter by 1 each tweet.
+                self.n = self.n + 1
+                # If the tweet count is still below the maximum then continue and print tweets.
+                if self.n < self.m:
+                    print('tweets = ' + str(self.n))
+                    # Write the tweet's information to the csv file.
+                    csvWriter.writerow([status.user.screen_name,
+                                        status.text.encode('utf-8'),
+                                        status.coordinates])
+
+                else:
+                    print('finished')
+                    return False
+            # If some error occurs.
+            except Exception as e:
+                # Print the error.
+                print(e)
+                # and continue.
+                pass
+
+        # Close the csv file.
+        csvFile.close()
+
+        # Return nothing.
+        return
+
+    # This changes the on_status class - when the twitter user posts a
+    # status related to the filters assigned this will catch it.
+    """
+    def on_status(self, status):
+        with open('Tweets.txt', 'a', encoding='utf-8', newline='') as f:
+            if not 'RT @'in status.text:
+                try:
+                    f.writerow([status.screen_name, status.text, status.coordinates])
+                    self.n = self.n + 1
+                    if self.n < self.m:
+                        print('tweets = ' + str(self.n))
+                        return True
+                    else:
+                        print('finished')
+                        return False
+                except Exception as e:
+                    # Print the error
+                    print(e)
+    """
+
+
+    # When the stream encounters an error of any kind this will print it out and
+    # keep the stream running.
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True
+
+    # If the stream times out then this method will post a message and keep running
+    # the stream.
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True
+
+
+# This defines the stream using the authorization settings and the stream listener.
+TweetStream = tweepy.streaming.Stream(auth, CustomStreamListener())
+
+# This is the filter for the stream, all the buzzwords and markers the user wants to
+# catch in statuses are put into here.
+TweetStream.filter(track=['chicken'])
---
+++ tokenTweets.py
@@ -0,0 +1,155 @@
+import json
+import re
+import csv
+import nltk
+import pandas as pd
+
+# The emoticon string to be used in regex expression, this defines the order or characters
+# to create the emoticon so it can be registered as 1 whole token.
+emoticons_str = r"""
+    (?:
+       [\>]? #eyebrows
+       [:=;8xXB] # Eye notations
+       [o0-]? # Noses
+       [\\\|/\)\(\]\[pPoODsS3\@$] # Mouths
+    )"""
+
+# This is the regex that will search for HTML tags, @-mentions, hashtags, URLS, words with 'and' and utf-8 bytes
+# for emoticons
+regex_str = [emoticons_str, r"(<[^>]+>)|(?:@[\w_]+)|"
+                            r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)|"
+                            r"(http)[s]?:[/]+.+(?='|\b)|"
+                            r"(?:[a-z][a-z'\-_]?|[0-9])+|"
+                            r"([\\x][a-z0-9]{3}){1,}"]
+
+
+# This compiles the regex. It allows for easier readibility using the VERBOSE flag which
+# allows the user to have comments within the regex and ignores the case of the letters using the IGNORECASE flag.
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+
+# This defines the function tokenise which will execute the regex and return all the tokens created by it from the
+# subject text.
+def tokenize(s):
+    """
+        Args:
+            :param s: string value
+        Returns:
+            Tokens object that contains created tokens from token regex.
+        """
+    return tokens_re.findall(s)
+
+
+# This defines the preprocess function which takes in the tokens and makes them uppercase. Then it runs the emoticon
+# regex on the tokens. After that is complete it checks if the text is in lowercase and if so it changes the token
+# value in the regex to be lowercase and then runs it.
+def preprocess(s, lowercase=False):
+    """
+    Args:
+        :param s: Brought in from tokenize
+        :param lowercase: Defaults the lower case to false
+    Returns:
+        Tokens object that contains created tokens from emoticon regex.
+    """
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+# This defines the run process which will take in the tweettext. It will try to create a column to put the processed
+# tokens in called val[0] and passes if it it fails.
+def run(text):
+    """
+        Args:
+            :param text: The tweet rows from the data.
+        Returns:
+            The processed tweets.
+        """
+    try:
+        processed = [val[0] for val in tokenize(text.lower())]
+    except:
+        pass
+
+    #Accesses the index and item within the enumerated processed text and if the item starts with \\
+    #the the process opens and finds rhe location of bytes to equal the item and then changes the
+    #value to the same rows description. it returns the newly processed token.
+    for idx, item in enumerate(processed):
+        if item[:1] == "\\":
+            try:
+                processed[idx] = foo.loc[foo["Bytes"] == item]["Description"].values[0]
+            except:
+                pass
+
+    return processed
+
+# This function was going to be used to iterate throught he directory and collect all the csv
+# files and combine them all into one and then run through tokenizing. However this was not
+# in the scope of this project.
+""" 
+fout = open("combine.csv", "a")
+#select first file - this may have to be a manual process
+for line in open("data_20170508151443.csv"):
+    fout.write(line)
+# This range is used to make sure everything from the year 2017 - 2018 is combined to one csv.
+for num in range(20170508151654,20180000000000):
+    f = open("data_"+str(num)+".csv")
+    f.next()
+    for line in f:
+        fout.write(line)
+    f.close()
+fout.close()
+"""
+
+# This reads in the unicdoe conversion csv using pandas.
+foo = pd.read_csv("Unicode_convert.csv")
+# This reads in the datafile with the tweets in using pandas.
+bar = pd.read_csv("data_20170508151654.csv")
+
+# This is the array tokenized tweets. for every row in the data the tokenized sents will run
+# the above functions.
+tokenized_sents = []
+for row in bar.iterrows():
+    tokenized_sents.append(run(row[1]["text"]))
+
+# This will print out the results.
+print(tokenized_sents)
+
+# This was a previous attempt at getting the conversion csv to replace the tweet value with the value of the same
+# description - this was an overthought process but was kept in just to show the working done to get to the correct
+# solution, or at least one that works at a basic level.
+"""
+desc = []
+bytes = []
+ctext = []
+
+with open('data_20170508151654.csv', 'r') as f, open('Unicode_convert.csv', 'r', ) as convert:
+    reader = csv.reader(f, delimiter=',')
+    reader2 = csv.reader(convert, delimiter=',')
+    next(convert)
+    for row in reader2:
+        bytes.append(row[1])
+        desc.append(row[2])
+    #print(bytes)
+    #print(desc)
+    next(f)
+    for row in reader:
+        ctext.append(row[1])
+        tokenized_sents = [preprocess(i) for i in ctext]
+        #for i in tokenized_sents:
+        #    print(i)
+print(tokenized_sents)
+"""
+
+# This piece of code was the initial tokenising code that did not take into account the fact that emoticons
+# would come out in utf-8 bytes and so was not used in the final release.
+"""
+ctext = []
+with open('data_20170508151654.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        ctext.append(row[1])
+        tokenized_sents = [preprocess(i) for i in ctext]
+        for i in tokenized_sents:
+            print(i)
+"""
